9a2ef54474acc0e1a8c841379c77eafc50968848
Sai Ritesh <sairiteshdomakuntla@gmail.com>
2025-01-18 17:18:36 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,13 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+import path from 'path';
+
 dotenv.config();
 
 const PORT=process.env.PORT || 5000;
+
+const __dirname=path.resolve();
 
 app.use(express.json()); //to parse the incoming requests with JSON payload (from req.body)
 app.use(cookieParser());
@@ -20,6 +24,12 @@ app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 
 server.listen(PORT,()=>{
     connectToMongoDB();
